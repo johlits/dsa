6dc1b485f7c0654114f44cf79000687fb1260565
johlits <2831146+johlits@users.noreply.github.com>
2025-09-22 09:56:33 +0200

    TextJustification

--- leetcode/Hard/TextJustification.cs
+++ leetcode/Hard/TextJustification.cs
@@ -11,35 +11,55 @@ namespace LeetCode.TextJustification
 
             for (var i = 0; i < words.Length + 1; i++)
             {
-                if (i < words.Length && bagLen + words[i].Length + 1 < maxWidth + 1)
+                if (i < words.Length && bagLen + words[i].Length < maxWidth + 1)
                 {
                     bag.Add(words[i]);
                     bagLen += words[i].Length + 1;
                 }
                 else
                 {
-                    bagLen -= bag.Count();
-                    var j = 0;
-                    while (bagLen < maxWidth)
-                    {
-                        bag[j++] += " ";
-                        bagLen++;
-                        if (j >= bag.Count() - 1)
-                        {
-                            j = 0;
-                        }
-                    }
-                    for (j = 0; j < bag.Count(); j++)
-                    {
-                        current += bag[j];
-                    }
-                    result.Add(current);
-                    bag = new List<string>();
-                    bagLen = 0;
-                    current = "";
                     if (i < words.Length)
                     {
+                        bagLen -= bag.Count();
+                        var j = 0;
+                        while (bagLen < maxWidth)
+                        {
+                            bag[j++] += " ";
+                            bagLen++;
+                            if (j >= bag.Count() - 1)
+                            {
+                                j = 0;
+                            }
+                        }
+                        for (j = 0; j < bag.Count(); j++)
+                        {
+                            current += bag[j];
+                        }
+                        result.Add(current);
+                        bag = new List<string>();
+                        bagLen = 0;
+                        current = "";
+
                         i--;
+                    }
+                    else
+                    {
+                        for (var j = 0; j < bag.Count() - 1; j++)
+                        {
+                            bag[j] += " ";
+                            bagLen++;
+                        }
+                        while (bagLen < maxWidth + bag.Count())
+                        {
+                            bag[bag.Count() - 1] += " ";
+                            bagLen++;
+                        }
+                        for (var j = 0; j < bag.Count(); j++)
+                        {
+                            current += bag[j];
+                        }
+                        result.Add(current);
+                        break;
                     }
                     
                 }
